e422ccc68b13510bcff1bf3e4c36b4db66274c90
Gracjan Puch <gracjanpuszek@gmail.com>
2020-12-08 17:02:55 +0100

    Day 8

---
+++ AoC_2020/Day08.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2020
+{
+	class Day08 : Day
+	{
+		public override void Solve()
+		{
+			StreamReader sr = new StreamReader("Inputs/day08.txt");
+			List<string> lines = new List<string>();
+			while (!sr.EndOfStream)
+				lines.Add(sr.ReadLine());
+
+			HashSet<int> indices = new HashSet<int>();
+
+			bool part1Found = false;
+
+			for (int j = -1; j < lines.Count; j++)
+			{
+				List<string> copy = new List<string>(lines);
+				if (j >= 0)
+				{
+					string[] l = copy[j].Split(' ');
+					if (l[0] == "acc")
+						continue;
+
+					copy[j] = ((l[0] == "nop") ? "jmp " : "nop ") + l[1];
+				}
+
+
+				int acc = 0;
+				int i;
+				int maxIters = copy.Count * 5;
+				for (i = 0; i < copy.Count;)
+				{
+					if (!part1Found)
+					{
+						if (!indices.Add(i))
+						{
+							Console.WriteLine($"Part 1: {acc}");
+							part1Found = true;
+						}
+					}
+
+					string[] l = copy[i].Split(' ');
+					switch (l[0])
+					{
+						case "acc":
+							acc += int.Parse(l[1]);
+							i++;
+							break;
+						case "nop":
+							i++;
+							break;
+						case "jmp":
+							i += int.Parse(l[1]);
+							break;
+					}
+					if (maxIters-- <= 0)
+						break;
+				}
+
+				if (i >= copy.Count)
+				{
+					Console.WriteLine($"Part 2: {acc}, changed index={j}");
+				}
+			}
+		}
+	}
+}
--- AoC_2020/Program.cs
+++ AoC_2020/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -16,7 +17,11 @@ namespace AoC_2020
 				Where(x => x.IsClass && x.BaseType == typeof(Day) && !x.IsAbstract && x.Namespace == "AoC_2020").
 				Select(x => (Day)Activator.CreateInstance(x)).ToList();
 
+			Stopwatch t = new Stopwatch();
+			t.Start();
 			days[days.Count - 1].Solve();
+			t.Stop();
+			Console.WriteLine($"Program took {t.ElapsedMilliseconds} ms");
 			Console.ReadLine();
 		}
 	}
